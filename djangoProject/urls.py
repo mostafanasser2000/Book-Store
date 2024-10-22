513adfe4ddb1a1a24784d6aac3c40a2c8d91df19
mostafanasser2000 <mustafanasser503@gmail.com>
2024-10-22 14:15:08 +0300

    feat: add required settings needed for production

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path("accounts/", include("allauth.urls")),
-    path("admin/", admin.site.urls),
+    path("anything-but-admin/", admin.site.urls),
     path("", include("pages.urls")),
     path("books/", include("books.urls")),
 ]
---
+++ generate_secret_key.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+import secrets
+print(secrets.token_urlsafe(38))
